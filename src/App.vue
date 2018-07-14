5e40f58de9e8e7d4abfe404ae644e745429877fe
hx-dl <546506879@qq.com>
2018-07-15 00:00:45 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <home/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import home from '@/pages/Home/home'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    home
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
